70c4de893ad4ce51daf54d3832abb98ac23d6128
ruanyandong <729368173@qq.com>
2020-04-24 14:13:45 +0800

    删除多余文件

--- app/src/main/java/com/ai/uploadbintray/MainActivity.java
+++ app/src/main/java/com/ai/uploadbintray/MainActivity.java
@@ -3,11 +3,14 @@ package com.ai.uploadbintray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ai.library.cmd;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String resul = cmd.exec();
     }
 }
